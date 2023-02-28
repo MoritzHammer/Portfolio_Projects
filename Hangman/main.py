718ac053bfad23e3c1ab5c1ae4beb02d10efe65b
admballuffvm <moritz.hammer@balluff.de>
2023-02-28 16:00:52 +0100

    Added an api

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,6 +1,5 @@
 import random
-# import time
-
+import requests
 
 def choose_word():
     with open('GermanWords.txt', 'r', encoding='UTF-8') as f:
@@ -24,7 +23,9 @@ def start():
 
 def choose_char():
     char = input("Please input a character: ").lower()
-    if char in word[1] and char not in guessed:
+    if char.lower() == "joker" and not has_won() and not is_dead():
+        print(f"A character in the word is: {joker(0)}")
+    elif char in word[1] and char not in guessed:
         word[1][char] = True
     else:
         guessed.append(char)
@@ -34,17 +35,32 @@ def choose_char():
 
     show_guessed_progress()
     if has_won():
+        fr = frequency()
         print("You won")
         print(f"You only guessed wrong {len(guessed)} times.")
+        print(f"The word '{word[0]}' appears in the internet a total of "
+              f"{format(fr[0], ',d').replace(',','.')} times of {fr[1]} entries."
+              f"\n That is {round(fr[0]/fr[1], 2)}%")
         another_game()
 
     elif not is_dead():
         choose_char()
 
     else:
+        fr = frequency()
         print("You are dead")
         print(f"The word was: '{word[0]}'")
+        print(f"The word '{word[0]}' appears in the internet a total of "
+              f"{format(fr[0], ',d').replace(',','.')} times of {fr[1]} entries."
+              f"\n That is {round(fr[0]/fr[1], 4)}%")
         another_game()
+
+
+def joker(i):
+    c = word[0][i]
+    if word[1][c.lower()] is True:
+        return joker(i + 1)
+    return c
 
 
 def show_guessed_progress():
@@ -63,6 +79,12 @@ def has_won():
         if not word[1][x]:
             return False
     return True
+
+
+def frequency():
+    url = f"https://www.dwds.de/api/frequency/?q={word[0]}"
+    response = requests.get(url=url).json()
+    return int(response["hits"]), int(response["total"])
 
 
 def another_game():
